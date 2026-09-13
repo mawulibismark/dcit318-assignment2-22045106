195c59f75a94e125dc86aad7d4041eac8f157fcc
Mawuli Bismark <mawulibismark7@gmail.com>
2026-09-13 12:46:41 +0000

    Adding Movable Objects

---
+++ MovableObjects/MovableObjects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Define the interface
+interface IMovable
+{
+    void Move();
+}
+
+// Car implements IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Bicycle implements IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        // Call Move() on each instance
+        car.Move();
+        bicycle.Move();
+    }
+}
